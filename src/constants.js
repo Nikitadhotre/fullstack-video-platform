c22aa9325e95d894ff2cbf8239bdd8f06d4e18aa
Nikita Dhotre <nikitadhotre1704@gmail.com>
2025-08-15 22:19:01 +0530

    update utils

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_Name = "fullstack_video_platform"
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./db/db.js"
+
+const app = express()
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error ",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port :${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection Failed!! ",err );
+    
+})
